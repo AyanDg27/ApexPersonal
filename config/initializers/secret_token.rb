abf6b639df5d7b7df23b483afb6a203f6441b3c3
AyanDg27 <dg.ayan91@gmail.com>
2013-10-15 11:49:15 -0700

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApexPersonal::Application.config.secret_key_base = '81a44cedc252a8f5d73358e2c2b82cbd16f9a6a8f55c9deb89b8bb4091e5256b5dd507dae7fd3ea29e2f7fc2f6550780d32d9245fdbb28f39758a348ac7571ee'
